2fd8a1efa069e7992a628ac04e3c0765a91972be
Louis Jacquelin <jacquelinlouis@gmail.com>
2022-04-14 13:58:20 +0200

    Implement note's deletion

--- app/src/main/java/com/jac/notes/list/ListCompose.kt
+++ app/src/main/java/com/jac/notes/list/ListCompose.kt
@@ -1,7 +1,11 @@
 package com.jac.notes.list
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
@@ -13,12 +17,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jac.notes.R
 import com.jac.notes.data.DataNote
 import com.jac.notes.ui.theme.NotesTheme
 
@@ -35,10 +44,22 @@ fun DefaultPreview() {
 }
 
 @Composable
-private fun NoteItem(note: DataNote, onClicked: (Int) -> Unit) {
-    Column(modifier = Modifier.clickable(onClick = { onClicked(note.id) })) {
-        Text(note.title)
-        Text(note.content)
+private fun NoteItem(note: DataNote, onClicked: (Int) -> Unit, onDestroyClicked: (Int) -> Unit) {
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.End) {
+        Column(modifier = Modifier
+            .weight(1F)
+            .clickable(onClick = { onClicked(note.id) })) {
+            Text(note.title)
+            Text(note.content)
+
+        }
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_delete_24),
+            contentDescription = "Delete the note",
+            alignment = Alignment.Center,
+            modifier = Modifier.clickable { onDestroyClicked(note.id) }
+        )
     }
 }
 
@@ -46,7 +67,8 @@ private fun NoteItem(note: DataNote, onClicked: (Int) -> Unit) {
 @Composable
 fun ListCompose(@PreviewParameter(MainComposePreviewParameterProvider::class) notes: List<DataNote>,
                 onNoteItemClicked: (Int) -> Unit = {},
-                onCreateClicked: () -> Unit = {}) {
+                onCreateClicked: () -> Unit = {},
+                onDestroyClicked: (Int) -> Unit = {}) {
     NotesTheme {
         // A surface container using the 'background' color from the theme
         Scaffold(
@@ -58,7 +80,7 @@ fun ListCompose(@PreviewParameter(MainComposePreviewParameterProvider::class) no
         ) {
             LazyColumn {
                 items(notes) { note ->
-                    NoteItem(note, onClicked = onNoteItemClicked)
+                    NoteItem(note, onClicked = onNoteItemClicked, onDestroyClicked)
                     Divider(color = Color.Gray)
                 }
             }
@@ -71,5 +93,9 @@ fun ListCompose(listViewModel: ListViewModel = viewModel(),
                 onNoteItemClicked: (Int) -> Unit = {},
                 onCreateClicked: () -> Unit = {}) {
     val notes = listViewModel.notes.collectAsState(emptyList())
-    ListCompose(notes.value, onNoteItemClicked, onCreateClicked)
+    ListCompose(notes.value, onNoteItemClicked, onCreateClicked,
+        onDestroyClicked = { id ->
+            listViewModel.delete(notes.value.findLast { it.id == id }!!)
+        }
+    )
 }
